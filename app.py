3fa126ae3ce5ff1abf5e45f5d25538eb09b06b66
Joyprakash Hela <jayhela3@gmail.com>
2026-06-24 15:10:29 +0530

    Changes in app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 from analysis import analyze_dataset
 from preprocessing import preprocess_data
 from trainer import train
+from predictor import predict
+import joblib
 
 st.set_page_config(
     page_title = 'Auto Binary Classifier',
@@ -32,7 +34,7 @@ if uploaded_file is not None:
 
     analyze_dataset(df, target_column)
 
-    x, y = preprocess_data(df, target_column)
+    x, y, feature_info = preprocess_data(df, target_column)
 
     st.subheader("Feature Matrix")
     st.dataframe(x.head())
@@ -44,7 +46,25 @@ if uploaded_file is not None:
     st.dataframe(x.head())
     st.write('Remaining missing values:', x.isnull().sum().sum())
 
-    results_df, best_model = train(x, y)
+    results_df = train(x, y, feature_info)
 
     st.subheader('Model Comparison')
     st.dataframe(results_df)
+
+    feature_names = joblib.load("feature_names.pkl")
+
+    st.header("Make Prediction")
+
+    user_input = {}
+
+    for col in feature_info['numerical_cols']:
+        user_input[col] = st.number_input(col)
+
+    for col in feature_info['categorical_col']:
+        user_input[col] = st.text_input(col)
+
+    input_df = pd.DataFrame([user_input])
+
+    if st.button('Predict'):
+        prediction = predict(input_df)
+        st.success(f"Prediction:{prediction}")
