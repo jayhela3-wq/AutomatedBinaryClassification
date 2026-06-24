3712435f8ef688c365f9d59efb5389bd8b4d27d8
Joyprakash Hela <jayhela3@gmail.com>
2026-06-24 15:04:12 +0530

    Changes in trainer.py

--- trainer.py
+++ trainer.py
@@ -1,4 +1,8 @@
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -9,49 +13,74 @@ import pandas as pd
 import joblib
 
 
-def train(x, y):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 2)
+def train(x, y,feature_info):
+
+    numerical_cols = feature_info[numerical_cols]
+    categorical_cols = feature_info[categorical_cols]
+
+    numeric_pipe = Pipeline([
+        ('imputer',SimpleImputer(strategy='median')),
+        ('scaler',StandardScaler)
+    ])
+    categorical_pipe = Pipeline([
+        ('imputer',SimpleImputer(strategy='most_frequent')),
+        ('encoder',OneHotEncoder(handle_unknown='ignore'))
+    ])
+
+    preprocessor = ColumnTransformer([
+        ('num',numeric_pipe,numerical_cols),
+        ('categ',categorical_pipe,categorical_cols)
+    ])
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
 
     models = {
-        "Logistic Regression" : LogisticRegression(max_iter=1000),
-        "Decision Tree": DecisionTreeClassifier(random_state=2),
-        "Random Forest" : RandomForestClassifier(random_state=2),
-        "KNN" : KNeighborsClassifier(),
-        "SVM" : SVC(random_state=2)
+        'Logistic Regression' : LogisticRegression(max_iter=1000),
+        'Decision Tree' : DecisionTreeClassifier(random_state=42),
+        'Random Forest' : RandomForestClassifier(random_state=42),
+        'KNN' : KNeighborsClassifier(),
+        'SVM' : SVC()
     }
 
     results = []
-
     best_model = None
     best_f1 = 0
 
-    for name, model in models.items():
-        model.fit(x_train,y_train)
-        predictions = model.predict(x_test)
+    for name, classifier in models.items():
+
+        pipeline = Pipeline([
+            ('preprocessor',preprocessor),
+            ('classifier',classifier)
+        ])
+
+        pipeline.fit(x_train, y_train)
+
+        predictions = pipeline.predict(x_test)
+
         accuracy = accuracy_score(y_test, predictions)
-
-        precision = precision_score(y_test, predictions, zero_division = 0)
-
+        precision = precision_score(y_test, predictions, zero_division=0)
         recall = recall_score(y_test, predictions, zero_division=0)
-
         f1 = f1_score(y_test, predictions, zero_division=0)
 
         results.append({
-            'Model' : name,
-            "Accuracy" : accuracy,
-            "Precision" : precision,
-            "Recall" : recall,
-            "F1 Score" : f1
+            'Model':name,
+            'Accuracy':round(accuracy,2),
+            'Precision':round(precision,2),
+            'Recall':round(recall,2),
+            'F1_Score' : round(f1,2)
         })
 
-        if f1 > best_f1:
+        if f1 > best_f1 :
             best_f1 = f1
-            best_model = model
+            best_model = pipeline
 
+    
     results_df = pd.DataFrame(results)
 
-    joblib.dump(best_model, "best_model.pkl")
-    
-    return results_df, best_model
+    joblib.dump(best_model,"best_model.pkl")
+    joblib.dump("feature_info","feature_info.pkl")
+
+    return results_df
+
 
 
