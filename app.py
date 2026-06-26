b467e6ffe3e4728a763449de02a1d5ca21be000d
Joyprakash Hela <jayhela3@gmail.com>
2026-06-26 12:27:36 +0530

    More more close to final stage

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ if uploaded_file is not None:
 
     if st.button('Predict'):
         prediction = predict(input_df)
-        st.success(f"Prediction:{prediction}")
+        st.success(f"Prediction: {prediction}")
